38d756eac1b041ed9f756bda49b5333c2c09e1df
TranceLin <trancelin@cmlove.cc>
2018-09-10 18:32:14 +0800

    首页根据storeid获取notice

--- addons/ewei_shopv2/core/mobile/index.php
+++ addons/ewei_shopv2/core/mobile/index.php
@@ -145,7 +145,7 @@ class Index_EweiShopV2Page extends MobilePage
 			}
 		}
 		$goodsstyle = $_W['shopset']['shop']['goodsstyle'];
-		$notices = pdo_fetchall('select id, title, link, thumb from ' . tablename('ewei_shop_notice') . ' where uniacid=:uniacid and iswxapp=0 and status=1 order by displayorder desc limit 5', array(':uniacid' => $uniacid));
+		$notices = pdo_fetchall('select id, title, link, thumb from ' . tablename('ewei_shop_notice') . ' where uniacid=:uniacid and iswxapp=0 and status=1 and storeid like "%,":store_id",%" order by displayorder desc limit 5', array(':uniacid' => $uniacid,':store_id'=>$this_store_id));
 		$seckillinfo = plugin_run('seckill::getTaskSeckillInfo');
 		ob_start();
 		ob_implicit_flush(false);
