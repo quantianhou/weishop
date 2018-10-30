7bc98c177169933f112231d45f786b9657c17f29
TranceLin <trancelin@cmlove.cc>
2018-10-31 02:59:04 +0800

    debug---推荐商品

--- addons/ewei_shopv2/core/mobile/index.php
+++ addons/ewei_shopv2/core/mobile/index.php
@@ -154,7 +154,7 @@ class Index_EweiShopV2Page extends MobilePage
 			if (!(empty($goodsids)))
 			{
 //				$indexrecommands = pdo_fetchall('select id, title, thumb, marketprice,ispresell,presellprice, productprice, minprice, total from ' . tablename('ewei_shop_goods') . ' where id in( ' . $goodids . ' ) and uniacid=:uniacid and status=1 order by instr(\'' . $goodids . '\',id),displayorder desc', array(':uniacid' => $uniacid));
-				$indexrecommands = pdo_fetchall('select bg.title as bgtitle , bg.thumb as bgthumb , sg.* from '. tablename('ewei_business_goods') .' as bg right join '. tablename('ewei_shop_goods') .' as sg on bg.id = sg.business_goods_id where bg.id in ('. implode(',' , $goodsids).') and sg.uniacid=:uniacid and sg.status=1 order by instr(\'' . implode(',',$goodsids) . '\',bg.id), sg.displayorder desc', array(':uniacid' => $uniacid));
+				$indexrecommands = pdo_fetchall('select bg.title as bgtitle , bg.thumb as bgthumb , sg.* from '. tablename('ewei_business_goods') .' as bg right join '. tablename('ewei_shop_goods') .' as sg on bg.id = sg.business_goods_id where bg.id in ('. implode(',' , $goodsids).') and sg.uniacid=:uniacid and sg.shop_id = '. $this_store_id.' and sg.status=1 order by instr(\'' . implode(',',$goodsids) . '\',bg.id), sg.displayorder desc', array(':uniacid' => $uniacid));
 //				var_dump($indexrecommands);exit;
 				foreach ($indexrecommands as $key => $value )
 				{
