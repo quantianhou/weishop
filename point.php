5d86804d234c4452f00174f8dbc3f18f59f09f6b
Fanhailong <121838942@qq.com>
2018-11-23 23:12:11 +0800

    调通会员积分同步回调接口

--- point.php
+++ point.php
@@ -9,7 +9,7 @@ file_put_contents('./data/logs/point-javacallback-'.date('Ymd').'.log',date('Y-m
 file_put_contents('./data/logs/point-javacallback-'.date('Ymd').'.log',json_encode($_GPC['__input'])."\r\n",FILE_APPEND);
 // $_GPC['__input'] = '{"success":true,"code":"200","msg":"\u6ce8\u518c\u6210\u529f","data":{"companyNo":"100003","cardId":"erp55338","integration":5}}';
 if (isset($_GPC['__input']) && !empty($_GPC['__input'])) {
-    $info = json_decode($_GPC['__input'], true);
+    $info = $_GPC['__input'];
     if (!empty($info) && isset($info['success']) && $info['success']
         && isset($info['code']) && $info['code'] == 200
         && isset($info['data']) && !empty($info['data'])) {
